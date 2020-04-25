d78829e2fd4776b39a3a551567b968fc2403b016
Lily Gao <wjgao@edu.uwaterloo.ca>
2020-04-25 14:14:12 -0400

    use js json nicety

--- src/controllers/Registration.js
+++ src/controllers/Registration.js
@@ -57,7 +57,7 @@ class Registration {
       Registration.findDocFromId(patientId, PatientModel, (patient) => {
         patient.organization = orgId;
         Registration.defaultDocSave(patient, () => {
-          return res.status(200).send(`{"patient": ${patient}}`);
+          return res.status(200).send({ patient });
         }); // todo technically all calls to defaultDocSave should also be nested...
       });
     } catch (error) {
@@ -80,7 +80,7 @@ class Registration {
                 return res.status(200).send(`{"patient": ${patient}, "consultation": ${consultation}}`);
               });
             } else {
-              return res.status(200).send(`{"patient": ${patient}}`);
+              return res.status(200).send({ patient });
             }
           });
         });
@@ -113,7 +113,7 @@ class Registration {
             } else if (consultation.practitioner == null) {
               consultation.practitioner = practitionerId;
               Registration.defaultDocSave(consultation, () => {
-                return res.status(200).send(`{"consultation": ${consultation}}`);
+                return res.status(200).send({ consultation });
               });
             } else { // an active consultation exists, practitioner not null => patient already has practitioner
               return res.status(400).send(`{"error": "Bad request: patient with id ${patientId} is already registered with practitioner id ${consultation.practitioner} on active consultation id ${consultation.id}. No updates were performed."}`);
@@ -136,7 +136,7 @@ class Registration {
         if (consultation != null && consultation.practitioner != null) {
           consultation.active = false;
           Registration.defaultDocSave(consultation, () => {
-            return res.status(200).send(`{"consultation": ${consultation}}`);
+            return res.status(200).send({ consultation });
           });
         } else {
           return res.status(200).send('{"message": "patient was not registered to practitioner. No updates performed."}');
@@ -225,85 +225,6 @@ class Registration {
     // for all: return 200 for success, 400 if bad request, 500 if other error
 
    */
-  /*todo flows -- old
-    patient/whatsapp -> register to org -> { (assume org id already obtained)
-      - find patient doc from phone num
-      - set org field on patient
-    }
-    pract/socket -> register to patient =  { (assume pract id already obtained)
-      - find patient id from phone num
-      - Try to find active consultation
-        - if active consultation without pract field (=waiting room) and set pract
-        - (shouldn't happen?) if active consultation with pract already set:
-          - set active = false
-          - create new consultation like If none below
-        - If none:
-          - find practitioner org from pract id
-          - create new consultation with pract, org, patient, active=true, messages =[], default accepted_timestamp
-    }
-    patient/whatsapp -> deregister from pract = {
-      - find patient id from phone num
-      - Try to find active consultation
-        -  (shouldn't happen?) if none OR active consultation without pract, do nothing
-        - else set active = false
-    }
-    pract/socket -> deregister from patient = { (assume pract id, consultation id already obtained)
-      - find patient id from phone num // or do we have patient id already...?
-      - Get active consultation with pract
-        - set active = false
-    }
-    patient/whatsapp -> deregister from org = {
-      - find patient doc from phone num
-      - set patient org to null
-      - Try to find active consultation
-        - if found, set active = false
-        - else do nothing
-    }
-    // skipping org -> deregister from patient for now
-   */
-
-
-
-
-  // todo  to deregister with an org, practitioner must be deregistered first.
-//   static updatePatientOrg(patientId, orgId) {
-//     const updateFields = orgId != null ? {'organization': orgId} : {'organization': orgId, 'practitioner': null, 'active': false}
-//     ConsultationModel.findOneAndUpdate({ 'patient': patientId, 'active': true},
-//         updateFields,
-//         function (err, consultation) {
-//       if (err) return throw err; // todo deal with error
-//       // todo what to do after update?
-//       console.log('consultation successfully updated!');
-//
-//     })
-//   }
-//   static updatePatientPractitioner(patientId, practitionerId) {
-//     ConsultationModel.findOne({ 'patient': patientId, 'active': true},
-//        "organization practitioner active patient", //todo do we need to fetch a firld to update it
-//         function (err, consultation) {
-//           if (err) return throw err;
-//           consultation.practitioner = practitionerId;
-//           consultation.active = practitionerId != null
-//           consultation.save(function(err) {
-//                 if (err) throw err;
-//                 console.log('consultation successfully updated!');
-//           });
-//           if (!active){
-//             //clone consultation...
-//             consultation._doc._id = mongoose.Types.ObjectId();
-//             consultation.isNew = true;
-//             consultation.active = true;
-//
-// // save the user
-//             newUser.save(function(err) {
-//               if (err) throw err;
-//
-//               console.log('User created!');
-//             });
-//           }
-//           // todo what to do after update?
-//         })
-//   }
 }
 
 export default Registration;
