cf4f6a011fa7ee3b08c2d655d8d7dbef6c596d5d
Sam Wilks <sam.wilk92@gmail.com>
2020-04-25 00:13:44 -0400

    Added initial models

---
+++ src/models/bot.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+export const ValidationOperator = Object.freeze({
+    Is: 'is',
+    IsNot: 'is_not',
+    GreaterThan: 'gt',
+    LessThan: 'lt',
+    EqualTo : "eq",
+    NotEqualTo : "neq",
+    GreaterThanOrEqualTo: 'gte',
+    LessThanOrEqualTo : 'lte',
+    Contains: 'contains'
+})
+
+export const ContentTypes = Object.freeze({
+    Location: 'location',
+    Text: 'text',
+    Picture: 'picture',
+    Contact: 'contact',
+    Video: 'video',
+    Audio: 'audio',
+    Document: 'document'
+})
+
+const ValidationSchema = mongoose.model('validation', {
+    type: {
+        type: String,
+        enum: Object.values(ValidationOperator)
+    },
+    value: mongoose.Mixed,
+});
+
+const ResponseSchema = mongoose.model('response', {
+    value: String,
+    description: String,
+    points_to: mongoose.Schema.Types.ObjectId, 
+});
+
+const BotMessageSchema = mongoose.model('bot_message', {
+    text: String,
+    description: String,
+    validation:[validation], //validation on possible response. By default any responses that don't match the embedded responses will be rejected
+    responses: [ResponseSchema],
+});
